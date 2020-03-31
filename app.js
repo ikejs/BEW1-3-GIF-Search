869105eb90c7678a1246a89ac869d201e9e1b758
Ike <ikescode@gmail.com>
2020-03-31 12:38:13 -0500

    add working GIF search

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
+require('dotenv').config({ path: '.env' });
 const app = require('express')();
 const exphbs  = require('express-handlebars');
+const Tenor = require("tenorjs").client({
+  "Key": process.env.TENOR_API_KEY,
+  "Filter": "high",
+  "Locale": "en_US"
+});
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  console.log(req.query);
-  res.render('home');
-});
-
-app.get('/greetings/:name', (req, res) => {
-  const name = req.params.name;
-  res.render('greetings', { 
-    name
-  });
+  term = ""
+  if (req.query.term) {
+      term = req.query.term
+  }
+  Tenor.Search.Query(term, "10")
+      .then(response => {
+          const gifs = response;
+          res.render('home', { gifs })
+      }).catch(console.error);
 });
 
 app.listen(3000, () => {
