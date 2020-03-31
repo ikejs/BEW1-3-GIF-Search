d72c5c040d8418156dd3aef51b2c8762e1a67ccc
Ike <ikescode@gmail.com>
2020-03-31 12:22:54 -0500

    add form

--- app.js
+++ app.js
@@ -5,10 +5,8 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-  res.render('hello-gif', { 
-    gifUrl
-  });
+  console.log(req.query);
+  res.render('home');
 });
 
 app.get('/greetings/:name', (req, res) => {
